d38ce6289ecd9764f3550a61648af3a4e795388e
Chris Britton <Christopher.L.Britton@outlook.com>
2022-10-05 16:04:26 -0400

    Add Category model (pre data annotations)

---
+++ BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿namespace BulkyBookWeb.Models
+{
+    public class Category
+    {
+        // Building out what the DB table should look like
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+
+        // Setting the default create date time to DateTime.Now
+        public DateTime CreateDateTime { get; set; } = DateTime.Now;
+    }
+}
